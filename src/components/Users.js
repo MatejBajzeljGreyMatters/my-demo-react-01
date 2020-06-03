9dcf8ee2ef863c1c4a2b3d1ea660a92eed15d087
MatejBajzeljGreyMatters <61779853+MatejBajzeljGreyMatters@users.noreply.github.com>
2020-06-03 15:44:41 +0200

    Show list of users
    I used mocked REST service 
    https://mockapi.io.
    
    api path "users" which has to return array of objects with properties "id" and "name" atm.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,13 +1,47 @@
 import React, { Component } from "react";
+import axios from "axios";
 
-class Meetups extends Component {
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount = async () => {
+    await this.getUsers();
+    console.log("Users in state", this.state.users);
+  };
+
+  getUsers = async () => {
+    try {
+      const res = await axios.get(
+        "https://5ed7873c152c310016d851c7.mockapi.io/users"
+      );
+      console.log("Users response from axios", res);
+      this.setState({ users: res.data }, () => {
+        console.log("Get from state in calback", this.state);
+      });
+      // return users.data;
+    } catch (error) {
+      console.log("Users error from axios", error);
+      //return null;
+    }
+  };
+
   render() {
+    const users = this.state.users.map((user, i) => {
+      return <li key={user.id}>{user.name} </li>;
+    });
+
     return (
       <div>
         <h1>Users</h1>
+        {users}
       </div>
     );
   }
 }
 
-export default Meetups;
+export default Users;
